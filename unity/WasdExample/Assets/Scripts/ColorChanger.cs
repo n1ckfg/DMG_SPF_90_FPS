c8968edc8044bce4be548f57cf1082095ce1736c
Nick Fox-Gieg <nick@fox-gieg.com>
2017-08-08 20:44:30 -0400

    raycaster example

---
+++ unity/WasdExample/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+	public Color newColor;
+	private Raycaster raycaster;
+	private Renderer ren;
+	private Color origColor;
+
+	void Awake() {
+		ren = GetComponent<Renderer>();
+		origColor = ren.material.color;
+		raycaster = Camera.main.GetComponent<Raycaster>();
+	}
+	
+	void Update() {
+		if (raycaster.foundTagName) {
+			ren.material.color = newColor;
+		} else {
+			ren.material.color = origColor;
+		}
+	}
+
+}
