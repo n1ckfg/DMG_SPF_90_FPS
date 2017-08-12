7963b8c75e128d480438585ce4c206c649998a37
Nick Fox-Gieg <nick@fox-gieg.com>
2017-08-12 19:18:15 -0400

    unity class example

---
+++ unity/class_example/Assets/Scripts/Dot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+
+	public float scale = 100f;
+	public float spread = 10f;
+	public Color clickColor;
+	public Color hoverColor;
+
+	private BasicController ctl;
+	private Renderer ren;
+	private Color origColor;
+
+	void Awake() {
+		ren = GetComponent<Renderer>();
+		ctl = Camera.main.GetComponent<BasicController>();
+	}
+
+	void Start() {
+		origColor = ren.material.color;
+		scale = Random.Range(scale - spread, scale + spread);
+	}
+
+	void Update() {
+		transform.Translate(0f, Mathf.Sin(Time.realtimeSinceStartup)/scale, 0f);
+
+		if (ctl.isLookingAt == gameObject.name && ctl.isDrawing) {
+			ren.material.color = clickColor;
+		} else if (ctl.isLookingAt == gameObject.name && !ctl.isDrawing) {
+			ren.material.color = hoverColor;
+		} else {
+			ren.material.color = origColor;
+		}
+	}
+
+}
---
+++ unity/class_example/Assets/Scripts/DotsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotsManager : MonoBehaviour {
+
+	public Dot dotPrefab;
+	public int numDots = 50;
+
+	[HideInInspector] public List<Dot> dots;
+
+	private float spread = 1f;
+
+	private void Start() {
+		dots = new List<Dot>();
+		setSpread();
+		for (int i=0; i<numDots; i++) {
+			Vector3 p = new Vector3(Random.Range(-spread, spread), Random.Range(-spread, spread), Random.Range(-spread/4f, spread/4f));
+			Dot d = Instantiate(dotPrefab, p, Quaternion.identity);
+			d.transform.SetParent(transform);
+			d.name = "Dot" + (i+1);
+			dots.Add(d);
+		}
+	}
+
+	private void setSpread() {
+		spread = Mathf.Abs(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f)).z);
+	}
+
+}
